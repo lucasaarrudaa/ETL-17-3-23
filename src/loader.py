32a3f8f72d12d4b59b95b2ac6a3ce7eab684f7d3
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-04-02 21:39:47 -0300

    I changed the connection, from mysql to postgresql

--- src/loader.py
+++ src/loader.py
@@ -1,24 +1,51 @@
-from conf.connection import Connection
-import pandas as pd
+import psycopg2
 
 class Loader:
-    '''
-    Loading to DB.
-    '''
-    def load_to_db(self, schema, table_name, df):
+    
+    def __init__(self):
+        
+        self.host = 'localhost'
+        self.port = 15432
+        self.dbname = 'campaign'
+        self.user = 'postgres'
+        self.password = 'Postgres'
+        self.table_name = 'advertisings'
+        self.conn = None
+        self.cursor = None
 
-        dt=pd.DataFrame()
-        dt=df
-        try:
-            conn=Connection()
-            engine=conn.sql_engine()
-            dt.to_sql(
-                name=table_name,
-                 con=engine,
-                 schema=schema,
-                 if_exists='append',
-                 index=False
-                 )
-            print('Upload has sucessfully')
-        except Exception as e:
-            raise(e)
+        
+    def connect(self):
+        
+        self.conn = psycopg2.connect(
+            host=self.host,
+            port=self.port,
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password
+        )
+        self.cursor = self.conn.cursor()
+        print("Connected to database.")
+        
+    def upload_dataframe(self, dataframe):
+        
+        print('Starting upload...')
+        # Cria lista de tuplas a partir do dataframe
+        data = [tuple(row) for row in dataframe.to_numpy()]
+        
+        # Monta string de placeholders (?, ?, ?...)
+        placeholders = ', '.join(['%s'] * len(data[0]))
+        
+        # Cria comando SQL
+        sql = f"INSERT INTO {self.table_name} VALUES ({placeholders})"
+        
+        # Executa comando SQL com os dados
+        self.cursor.executemany(sql, data)
+        self.conn.commit()
+        
+        print("Upload successful!")
+        
+    def disconnect(self):
+        
+        self.cursor.close()
+        self.conn.close()
+        print("Disconnected from database.")
