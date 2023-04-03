5e046dfc39aad8774e1480cb651549b285c422d9
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-04-03 12:14:58 -0300

    changing some function returns

--- src/transformer.py
+++ src/transformer.py
@@ -25,6 +25,7 @@ class Transformer:
                  'signed_at':'object',
                  'revenue':'object'}
         dt=dt.astype(dtype=dtypes)
+        
         return dt
         
     def extract_string(self, table, column, regex):
@@ -40,6 +41,7 @@ class Transformer:
                 Return what do you want to extract.
         '''
         extracted = table[f'{column}'].str.extract('({})'.format(regex))
+        
         return extracted
 
     def delete_col(self, df, column):
@@ -51,10 +53,13 @@ class Transformer:
                 column: column to delete
         NOTE: you need to assign to a df. 
         '''
-        return df.loc[:, ~df.columns.isin([f'{column}'])]
+        deleted = df.loc[:, ~df.columns.isin([f'{column}'])]
+        
+        return deleted
     
     def rename(self, df, column1, new_name):
         df = df.rename(columns = {f'{column1}':f'{new_name}'}, inplace = True)
+        
         return df
         '''
         Renaming a column from a DF.
@@ -64,7 +69,9 @@ class Transformer:
         '''
         Concat dfs.
         '''
-        return pd.concat([df1, df2], ignore_index = ignore_index)
+        concatenated = pd.concat([df1, df2], ignore_index = ignore_index)
+        
+        return concatenated
 
         
     def join(self, new_df, df1, df2):
@@ -72,6 +79,7 @@ class Transformer:
         Join columns and attribute to new DF.
         '''
         new_df = df1.join(df2, rsuffix='_r', lsuffix='_l')
+        
         return new_df
     
     def drop_duplicates(self, df, col):
@@ -85,6 +93,7 @@ class Transformer:
                 df without specified column
         '''
         dt = df.drop_duplicates([f'{col}'])
+        
         return dt
     
     def fill(self, df, rows, value):
@@ -101,4 +110,5 @@ class Transformer:
         '''
         self.rows = np.full(rows, value)
         new_df = df.assign(advertising=value) #NOTE: you need to change 'advertising' to your preferenced name column
+        
         return new_df
