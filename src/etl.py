fdceead20c5341b9114ec720986a025f080f3252
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-04-12 22:36:02 -0300

    update

--- src/etl.py
+++ src/etl.py
@@ -7,13 +7,11 @@ class Etl:
             
     def __init__(self):    
         
-        # Extracting from bucket to local
         leads = ExtractorS3('datalake-my-lucas-bucket').download('data/customer_leads_funnel.csv', 'datasets/customer_leads_funnel.csv')
         fb = ExtractorS3('datalake-my-lucas-bucket').download('data/facebook_ads_media_costs.jsonl', 'datasets/facebook_ads_media_costs.jsonl')
         pv = ExtractorS3('datalake-my-lucas-bucket').download('data/pageview.txt','datasets/pageview.txt')
         ggl = ExtractorS3('datalake-my-lucas-bucket').download('data/google_ads_media_costs.jsonl','datasets/google_ads_media_costs.jsonl')
 
-        # Columns to dfs
         cols2 = ['ips',
                 'device_id',
                 'refer']
@@ -31,7 +29,6 @@ class Etl:
         df_fb = pd.read_json(fb, lines=True)
         df_ggl = pd.read_json(ggl, lines=True)
 
-        # Cleaning columns
         transforms = [
             ('ip', 'ips', '\d{3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'),
             ('device_id', 'device_id', '\:\s.+'),
@@ -54,7 +51,6 @@ class Etl:
 
         df_fb = Transformer().delete_col(df_fb, 'facebook_campaign_id')
 
-        # Renaming columns
         renames = [
             (df_pv, 'data', 'data_click'),
             (df_pv, 'click', 'campaign_link'),
@@ -67,17 +63,14 @@ class Etl:
         for df, old_name, new_name in renames:
             Transformer().rename(df, old_name, new_name)
 
-        # Assigning values ​​to the variables to populate the new DF
         df_fb = Transformer().fill(df_fb, 460, 'http://www.facebook.com')
         df_ggl = Transformer().fill(df_ggl, 5796, 'http://google.com.br')
 
-        # Concatening DFS
         advertisings = Transformer().concat(df_fb, df_ggl)
         leads_data = Transformer().concat(df_pv, df_leads)
         table = pd.concat([advertisings, leads_data])
         table.fillna(0, inplace=True)
 
-        # Uploading DF to DB
         table = Transformer().table_maker(table)
 
         load = Loader()
