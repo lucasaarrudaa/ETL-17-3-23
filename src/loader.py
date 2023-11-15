ae0a534e9f9f88a9d3a0c2da3565dc12ef7b7089
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-11-15 15:46:01 -0300

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -9,18 +9,13 @@ class Loader:
         self.connector.connect()
         try:
             print('Starting upload...')
-            # Create list of tuples from dataframe
             data = [tuple(row) for row in dataframe.to_numpy()]
 
             placeholders = ', '.join(['%s'] * len(data[0]))
-
-            # Create SQL command
             sql = f"INSERT INTO {self.table_name} VALUES ({placeholders})"
 
-            # Run SQL command with data
             self.connector.cursor.executemany(sql, data)
             self.connector.conn.commit()
-
             print("Upload successful!")
         except psycopg2.Error as e:
             self.connector.conn.rollback()
