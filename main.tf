68f3d5a6f3eae9ad0b528ff968c8f0569b7f52d0
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-11-15 15:59:33 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "bucket_dl" {
+    bucket = "bucket-lucas-las"
+    acl="private"
+
+    server_side_encryption_configuration {
+        rule{
+            apply_server_side_encryption_by_default{
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
+
+provider "aws"{
+    region="us-east-1"
+}
