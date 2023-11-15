47ec3a28f43b6628b6b1c145b1aa7e9ef9a6fadb
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-11-15 15:47:32 -0300

    Update extractor.py

--- src/extractor.py
+++ src/extractor.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import boto3
 
@@ -14,30 +15,27 @@ class ExtractorLocal:
         Read CSV
 
         Parameters:         
-            file_path: copy file path and paste here
-            columns (string): is optional, set the name of te columns
-            header (string): is optional
-            delimiter (string): is optional
+            file_path (string): The file path
+            columns (list): List of column names
+            header (int): Row number to use as the column names
+            delimiter (string): The delimiter used in the CSV file
         Returns: 
-            return the dataframe, when call the method, include to a variable to view.
+            DataFrame: Returns the DataFrame
         '''
-        csv = pd.read_csv(rf'{file_path}', names=columns, header=header, delimiter=delimiter)
-        
+        csv = pd.read_csv(file_path, names=columns, header=header, delimiter=delimiter)
         return csv 
 
     def json(self, file_path, lines_opt=True):
         '''
-        Read json 
+        Read JSON
 
         Parameters:         
-            file_path (string): copy file path
-            lines_opt (bool): optional
+            file_path (string): The file path
+            lines_opt (bool): If True, reads the file as a JSON lines format
         Returns: 
-            return the dataframe, when call the method, include to a variable to view.
-
+            DataFrame: Returns the DataFrame
         '''
-        json = pd.read_json(f'{file_path}', lines=lines_opt)
-        
+        json = pd.read_json(file_path, lines=lines_opt)
         return json
 
 
@@ -47,13 +45,6 @@ class ExtractorS3:
     '''
 
     def __init__(self, name):
-        '''
-        Connecting to archive in s3 bucket.
-
-            Parameters: 
-                    name (string): The name of the bucket
-                    archive (string): The location of the archive
-        '''
         self.s3_client = boto3.client('s3')
         self.bucket_name = name
 
@@ -65,7 +56,7 @@ class ExtractorS3:
         bucket = s3.Bucket(self.bucket_name)
         files = []
         
-        for obj in bucket.objects.filter(Prefix=f'{path}'):
+        for obj in bucket.objects.filter(Prefix=path):
             files.append(obj.key)
             
         return files
@@ -74,70 +65,56 @@ class ExtractorS3:
         '''
         Downloading the archive from bucket to local path.
 
-            Parameters: 
-                local_path (string): Your path without the last ' / '
-                name (string): The name of archive
-                type (string): The type of the archive (csv, json, xml...)
-
-            Returns: 
-                    the file from S3 to your destination folder.
+        Parameters: 
+            s3_path (string): The path of the file in S3
+            local_path (string): The local path to save the downloaded file
+        Returns: 
+            bool: Returns True if the download was successful, else False
         '''
         try:
-            self.s3_client.download_file(
-                f'{self.bucket_name}', f'{s3_path}', f'{local_path}')
-        except:
-            print("Was not posible to download archive from bucket")
-            downloaded = self.s3_client.download_file(f'{self.bucket_name}', f'{s3_path}', f'{local_path}')
-
-        return downloaded
+            self.s3_client.download_file(self.bucket_name, s3_path, local_path)
+            return True
+        except Exception as e:
+            print(f"Was not possible to download archive from bucket: {e}")
+            return False
 
     def upload(self, local_file_path, bucket_path):
         '''
         Upload archive from local path to s3 bucket.
 
         Parameters: 
-                local_file_path (string) all file path (with archive)
-                bucket (string): nme of the bucket s3
-                bucket_path (string): destiny path of s3
-                name (string): name of new archive on s3
-                type (string): csv, json, xml...
+            local_file_path (string): File path in local storage
+            bucket_path (string): Destination path in S3 bucket
         Returns: 
-                returns the file from s3 to the folder you chose
+            bool: Returns True if the upload was successful, else False
         '''
         try:
-            self.s3_client.upload_file(f'{local_file_path}',
-                                  f'{self.bucket_name}', f'{bucket_path}')
-        except:
-            print("Was not posible to upload archive to bucket")
-            uploaded = self.s3_client.upload_file(f'{local_file_path}', f'{self.bucket_name}', f'{bucket_path}')
-            
-            return uploaded
+            self.s3_client.upload_file(local_file_path, self.bucket_name, bucket_path)
+            return True
+        except Exception as e:
+            print(f"Was not possible to upload archive to bucket: {e}")
+            return False
 
-    def csv(self, file_path, columns=None, header=None, delimiter=None):
+    def read_file_as_df(self, file_path, file_type='csv', **kwargs):
         '''
-        Open the archive (CSV) downloaded from s3 with a DF
+        Read file from S3 and return as DataFrame.
 
-        Parameter:         
-            optional (string): specific delimiter
-            optional2 (string): header = 0 or None (default)
-            file_path (string): copy file path imported from s3
-            columns (string): is optional, set the name of te columns
+        Parameters: 
+            file_path (string): The file path in S3
+            file_type (string): Type of file to read ('csv' or 'json')
+            **kwargs: Additional arguments to pass to pd.read_csv or pd.read_json
         Returns: 
-            return the dataframe, when call the method, include to a variable to view.
+            DataFrame: Returns the DataFrame
         '''
-        csv = pd.read_csv(rf'{file_path}', names=columns, header=header, delimiter=delimiter)
-        
-        return csv
-    
-    def json(self, file_path, lines_opt=True):
-        '''
-        Open the archive (JSON) downloaded from s3 with a DF
-
-        Parameter:         
-            file_path (string): copy file path imported from s3
-        Returns: 
-            return the dataframe, when call the method, include to a variable to view.
-        '''
-        json = pd.read_json(f'{file_path}', lines=lines_opt)
-        
-        return json
+        try:
+            local_file = f"/tmp/{os.path.basename(file_path)}"
+            if self.download(file_path, local_file):
+                if file_type == 'csv':
+                    return pd.read_csv(local_file, **kwargs)
+                elif file_type == 'json':
+                    return pd.read_json(local_file, **kwargs)
+                else:
+                    raise ValueError("Invalid file type. Supported types are 'csv' and 'json'.")
+        except Exception as e:
+            print(f"Error reading file as DataFrame: {e}")
+            return pd.DataFrame()
