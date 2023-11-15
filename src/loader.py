559eeecd5c7b692b7d59d4db5313363b0830c03f
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-11-15 15:45:46 -0300

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,50 +1,30 @@
-import os
-from dotenv import load_dotenv
 import psycopg2
 
 class Loader:
-    def __init__(self):
-        load_dotenv()  
-        self.host = os.getenv('DB_HOST')
-        self.port = int(os.getenv('DB_PORT'))
-        self.dbname = os.getenv('DB_NAME')
-        self.user = os.getenv('DB_USER')
-        self.password = os.getenv('DB_PASSWORD')
-        self.table_name = 'advertisings'
-        self.conn = None
-        self.cursor = None
-        
-    def connect(self):
-        try:
-            self.conn = psycopg2.connect(
-                host=self.host,
-                port=self.port,
-                dbname=self.dbname,
-                user=self.user,
-                password=self.password
-            )
-            self.cursor = self.conn.cursor()
-            print("Connected to database.")
-        except psycopg2.Error as e:
-            print(f"Error connecting to the database: {e}")
-        
+    def __init__(self, table_name='advertisings'):
+        self.table_name = table_name
+        self.connector = DatabaseConnector()
+
     def upload_dataframe(self, dataframe):
+        self.connector.connect()
         try:
             print('Starting upload...')
+            # Create list of tuples from dataframe
             data = [tuple(row) for row in dataframe.to_numpy()]
-            
+
             placeholders = ', '.join(['%s'] * len(data[0]))
+
+            # Create SQL command
             sql = f"INSERT INTO {self.table_name} VALUES ({placeholders})"
-            
-            self.cursor.executemany(sql, data)
-            self.conn.commit()
+
+            # Run SQL command with data
+            self.connector.cursor.executemany(sql, data)
+            self.connector.conn.commit()
+
             print("Upload successful!")
         except psycopg2.Error as e:
-            self.conn.rollback()
+            self.connector.conn.rollback()
             print(f"Error uploading data: {e}")
-        
-    def disconnect(self):
-        
-        self.cursor.close()
-        self.conn.close()
-        print("Disconnected from database.")
+        finally:
+            self.connector.disconnect()
+
