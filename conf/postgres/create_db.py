d6c47a244ad535be28bceb2e03eaae90c7c62c1a
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-11-15 15:53:36 -0300

    Update create_db.py

--- conf/postgres/create_db.py
+++ conf/postgres/create_db.py
@@ -1,33 +1,24 @@
 import psycopg2
 
 def create_db():
-    # Connects to the Docker database
-    conn = psycopg2.connect(
-        host="localhost",
-        port="15432",
-        database="postgres",
-        user="postgres",
-        password="Postgres"
-    )
+    connector = DatabaseConnector()
+    connector.connect()
     
-    # Sets autocommit to True
-    conn.autocommit = True
-    
-    # Checks if the database already exists
-    cur = conn.cursor()
-    cur.execute("SELECT 1 FROM pg_database WHERE datname='mydatabase'")
-    exists = cur.fetchone()
-    
-    # If the database doesn't exist, creates it
-    if not exists:
-        cur.execute("CREATE DATABASE mydatabase;")
-        print("Database created successfully!")
-    else:
-        print("Database already exists.")
-    
-    # Closes the connection
-    cur.close()
-    conn.close()
+    try:
+        # Checks if the database already exists
+        connector.cursor.execute("SELECT 1 FROM pg_database WHERE datname='mydatabase'")
+        exists = connector.cursor.fetchone()
+        
+        # If the database doesn't exist, creates it
+        if not exists:
+            connector.cursor.execute("CREATE DATABASE mydatabase;")
+            print("Database created successfully!")
+        else:
+            print("Database already exists.")
+    except psycopg2.Error as e:
+        print(f"Error creating database: {e}")
+    finally:
+        connector.disconnect()
 
 if __name__ == "__main__":
     create_db()
