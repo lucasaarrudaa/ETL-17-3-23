c3532f4f889be7dae7923087cf0f32f01e7ba1ec
Lucas Arruda <125474855+lucasaarrudaa@users.noreply.github.com>
2023-11-15 15:58:22 -0300

    Update create_table.py

--- conf/postgres/create_table.py
+++ conf/postgres/create_table.py
@@ -1,49 +1,42 @@
 import psycopg2
 
 def create_table():
+    connector = DatabaseConnector()
+    connector.connect()
     
     try:
-        conn = psycopg2.connect(
-            host="localhost",
-            port="15432",
-            database="mydatabase",
-            user="postgres",
-            password="Postgres"
-        )
-    except:
-        print("Unable to connect to database")
+        self.cursor.execute("SELECT 1 FROM pg_database WHERE datname=%s", (self.dbname,))
+        exists = self.cursor.fetchone()
 
-    cur = conn.cursor()
-
-    try:
-        cur.execute("""
-        campaign_date TIMESTAMP NULL,
-        campaign_name VARCHAR(50) NULL,
-        impressions INTEGER NULL,
-        clicks INTEGER NULL,
-        cost FLOAT NULL,
-        advertising VARCHAR(23) NULL,
-        ip VARCHAR(12) NULL,
-        device_id CHAR(11) NULL,
-        campaign_link VARCHAR(255) NULL,
-        data_click TIMESTAMP NULL,
-        lead_id CHAR(9) NULL,
-        registered_at TIMESTAMP NULL,
-        credit_decision CHAR(2) NULL,
-        credit_decision_at TIMESTAMP NULL,
-        signed_at TIMESTAMP NULL,
-        revenue FLOAT NULL
-    )
-    """)
+        if exists:
+            self.cursor.execute("""
+                CREATE TABLE IF NOT EXISTS your_table_name (
+                    campaign_date TIMESTAMP NULL,
+                    campaign_name VARCHAR(50) NULL,
+                    impressions INTEGER NULL,
+                    clicks INTEGER NULL,
+                    cost FLOAT NULL,
+                    advertising VARCHAR(23) NULL,
+                    ip VARCHAR(12) NULL,
+                    device_id CHAR(11) NULL,
+                    campaign_link VARCHAR(255) NULL,
+                    data_click TIMESTAMP NULL,
+                    lead_id CHAR(9) NULL,
+                    registered_at TIMESTAMP NULL,
+                    credit_decision CHAR(2) NULL,
+                    credit_decision_at TIMESTAMP NULL,
+                    signed_at TIMESTAMP NULL,
+                    revenue FLOAT NULL
+                )
+            """)
+            self.conn.commit()
+            print("Table created successfully!")
+        else:
+            print("Database does not exist.")
+    except psycopg2.Error as e:
+        print(f"Error creating table: {e}")
+    finally:
+        connector.disconnect()
         
-    except:
-        print("Unable to create table")
-
-    conn.commit()
-
-    cur.close()
-    conn.close()
-    print("Tables created successfully!")
-
 if __name__ == "__main__":
-    create_table()
+    connector.create_table()
